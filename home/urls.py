1ea5620807e17b8327b5541fb37038ccb21b6fed
eriksoftdev <eriksoftdev@gmail.com>
2026-03-16 00:29:58 -0600

    feat: Configurar login y proteger vistas de la aplicacion ventas

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.index)
+    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
--- ventas/views.py
+++ ventas/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def dashboard(request):
     return render(request, 'dashboard.html')
 
+@login_required
 def prepago(request):
     return render(request, 'prepago.html')
 
+@login_required
 def pospago(request):
     return render(request, 'pospago.html')
 
+@login_required
 def tarjetas(request):
     return render(request, 'tarjetas.html') 
