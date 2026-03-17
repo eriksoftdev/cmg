d1d25403563cd271f9a27c438480502f4f0d7d56
eriksoftdev <eriksoftdev@gmail.com>
2026-03-17 06:57:36 -0600

    feat: Se creo el modelo nota y se realizaron migraciones

--- notas/models.py
+++ notas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Nota(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
