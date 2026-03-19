7c1f8b9138dcd61012d5be9f80eade43e5e6ec2a
eriksoftdev <eriksoftdev@gmail.com>
2026-03-18 22:50:14 -0600

    feat: Se agrega funcionalidad para insertar notas en la bd y mostrarlos en la interfaz de notas

--- notas/views.py
+++ notas/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import NotaForm
+from .models import Nota
 
 # Create your views here.
 def index_notas(request):
     if request.method == 'GET':
+        my_notes = Nota.objects.filter(user=request.user).order_by('-created')
         return render(request, 'notas.html', {
-        'form': NotaForm()
+        'form': NotaForm(),
+        'notes': my_notes
     })
     else:
         form = NotaForm(request.POST)
